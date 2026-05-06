9b477bdd47ee028d9a86739487ee5d32334fcdfa
jango-blockchaiend <info@cryptolinx.de>
2026-05-06 06:19:31 +0200

    chore: remove unused vite-tsconfig-paths, @types/jest, jest-mock and update types.d.ts

--- test/types.d.ts
+++ test/types.d.ts
@@ -1,6 +1,5 @@
 declare module "bun:test" {
-  export const expect: typeof import("@jest/expect").expect;
-  export const jest: typeof import("jest-mock").jest;
+  export const expect: typeof import("bun:test").expect;
   export const test: (...args: any[]) => any;
   export const describe: (...args: any[]) => any;
   export const beforeEach: (...args: any[]) => any;

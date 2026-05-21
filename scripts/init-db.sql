d87830f357f16cf3e406cd990442502005e14ec2
jango-blockchained <info@cryptolinx.de>
2026-05-21 16:13:18 +0200

    chore: remove deprecated init-db.sql (use schema.sql instead)

--- scripts/init-db.sql
+++
@@ -1,5 +0,0 @@
--- This file is deprecated. Use ../schema.sql instead for D1 migrations.
--- Keeping for reference only.
-
--- The correct schema is in ../schema.sql
--- Run: bunx wrangler d1 migrations apply trade-data-db --workers/trade-worker 
